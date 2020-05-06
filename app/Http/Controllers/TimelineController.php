cdef3e5917c254b31dfae1eeb93a51f8afdbd087
MarioAlbertoBS <maralbo1@hotmail.com>
2020-05-06 09:33:26 -0700

    Changed default home page, added react preset

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class TimelineController extends Controller
 {
-    //
     public function index()
     {
         return view('home');
